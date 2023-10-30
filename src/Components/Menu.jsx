108782139f331ed3d390da1007373456abc7fb9f
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-10-30 13:46:42 +0100

    Prettier

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,27 +1,28 @@
-import HamburgerIcon from './HamburgerIcon';
-import style from './Menu.module.scss';
-import CrossIcon from './CrossIcon';
-import { AnimatePresence, motion } from 'framer-motion';
+import HamburgerIcon from "./HamburgerIcon";
+import style from "./Menu.module.scss";
+import CrossIcon from "./CrossIcon";
+import { AnimatePresence, motion } from "framer-motion";
 
 //TODO close on scroll
 
-
 function Menu({ onClick, isOpen }) {
-
   const menuVars = {
     init: { scaleY: 0 },
-    animate: { scaleY: 1, transition: {
-      duration: .5,
-      delayChildren: .5,
-      staggerChildren: .2
-    }},
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        delayChildren: 0.5,
+        staggerChildren: 0.2,
+      },
+    },
     exit: {
       scaleY: 0,
       transition: {
-        duration: .5
-      }
-    }
-  }
+        duration: 0.5,
+      },
+    },
+  };
 
   return (
     <motion.nav className={style.navbar}>
@@ -32,15 +33,23 @@ function Menu({ onClick, isOpen }) {
         {isOpen && (
           <motion.section
             variants={menuVars}
-            initial='init'
-            animate='animate'
-            exit='exit'
+            initial="init"
+            animate="animate"
+            exit="exit"
             className={style.openMenu}
           >
-            <motion.a variants={menuVars} href="/">Home</motion.a>
-            <motion.a variants={menuVars} href="/attractions">Attractions</motion.a>
-            <motion.a variants={menuVars} href="/">Tickets</motion.a>
-            <motion.a variants={menuVars} href="/">About us</motion.a>
+            <motion.a variants={menuVars} href="/">
+              Home
+            </motion.a>
+            <motion.a variants={menuVars} href="/attractions">
+              Attractions
+            </motion.a>
+            <motion.a variants={menuVars} href="/">
+              Tickets
+            </motion.a>
+            <motion.a variants={menuVars} href="/">
+              About us
+            </motion.a>
           </motion.section>
         )}
       </AnimatePresence>
