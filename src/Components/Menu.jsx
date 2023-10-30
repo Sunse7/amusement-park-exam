93feb857cfa610b6a29ab85f23858ffa195e1dc4
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-10-30 11:09:55 +0100

    Exit animation on menu, needs more tinkering

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,7 +2,7 @@ import HamburgerIcon from "./HamburgerIcon";
 import style from "./Menu.module.scss";
 import CrossIcon from "./CrossIcon";
 import { NavLink } from "react-router-dom";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 //TODO close on scroll, change navlink to a
 
@@ -12,23 +12,26 @@ function Menu({ onClick, isOpen }) {
       <button onClick={onClick}>
         {isOpen ? <CrossIcon /> : <HamburgerIcon />}
       </button>
-      {isOpen && (
-        <motion.section
-          initial={{ scaleY: 0 }}
-          animate={{
-            scaleY: 1,
-            transition: {
-              duration: 0.5,
-            },
-          }}
-          className={style.openMenu}
-        >
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/attractions">Attractions</NavLink>
-          <NavLink to="/">Tickets</NavLink>
-          <NavLink to="/">About us</NavLink>
-        </motion.section>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.section
+            initial={{ scaleY: 0 }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                duration: 0.5,
+              },
+            }}
+            exit={{ scaleY: 0 }}
+            className={style.openMenu}
+          >
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/attractions">Attractions</NavLink>
+            <NavLink to="/">Tickets</NavLink>
+            <NavLink to="/">About us</NavLink>
+          </motion.section>
+        )}
+      </AnimatePresence>
     </motion.nav>
   );
 }
