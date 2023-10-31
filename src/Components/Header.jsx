ae80061d6a66b24223500a18368526a3ab2c951b
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-10-31 15:31:33 +0100

    Close menu on scroll

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
-import style  from './Header.module.scss';
-import Menu from './Menu';
+import { useEffect, useState } from "react";
+import style from "./Header.module.scss";
+import Menu from "./Menu";
 
 function Header() {
-    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-    function toggleHamburger() {
-        setHamburgerOpen(!hamburgerOpen);
-    }
+  function toggleHamburger() {
+    setHamburgerOpen(!hamburgerOpen);
+  }
 
-    return ( 
-        <header className={style.header}>
-            <Menu onClick={toggleHamburger} isOpen={hamburgerOpen} />
-            <h1 className={style.header__title}>Alheimur</h1>
-        </header>
-     );
+  useEffect(() => {
+    const closeOnScroll = () => setHamburgerOpen(false);
+    window.addEventListener("scroll", closeOnScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", closeOnScroll);
+    };
+  }, []);
+
+  return (
+    <header className={style.header}>
+      <Menu onClick={toggleHamburger} isOpen={hamburgerOpen} />
+      <h1 className={style.header__title}>Alheimur</h1>
+    </header>
+  );
 }
 
 export default Header;
