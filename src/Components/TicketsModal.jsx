2cccb840a0bd7adeff9429ac31529da69e8217c1
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-10-31 13:12:54 +0100

    Change amount, open modal

--- src/Components/TicketsModal.jsx
+++ src/Components/TicketsModal.jsx
@@ -1,19 +1,38 @@
-import MyCalendar from './MyCalendar';
-import ConfirmButton from './ConfirmButton';
-import style from './TicketsModal.module.scss';
-import { motion } from 'framer-motion';
+import MyCalendar from "./MyCalendar";
+import ConfirmButton from "./ConfirmButton";
+import style from "./TicketsModal.module.scss";
+import { motion } from "framer-motion";
+import TicketAmount from "./TicketAmount";
+import { useState } from "react";
+import ConfirmModal from "./ConfirmModal";
 
 function TicketsModal() {
+  const [amount, setAmount] = useState(1);
+  const [isVisible, setIsVisible] = useState(false);
 
   const MotionConfirmButton = motion(ConfirmButton);
-  const darkGrey = '#1f1e1e';
+  const darkGrey = "#1f1e1e";
+
+  function onBuyTicket() {
+    setIsVisible(true);
+  }
 
   return (
     <section className={style.modal}>
       <p>Buy your tickets here</p>
       <section>
         <MyCalendar />
-        <MotionConfirmButton whileTap={{ scale: 0.9 }} whileHover={{ color: darkGrey }} />
+        <TicketAmount
+          onMinusClick={() => setAmount(amount - 1)}
+          onPlusClick={() => setAmount(amount + 1)}
+          amount={amount}
+        />
+        <MotionConfirmButton
+          onClick={onBuyTicket}
+          whileTap={{ scale: 0.9 }}
+          whileHover={{ color: darkGrey }}
+        />
+        {isVisible && <ConfirmModal numTickets={amount} />}
       </section>
     </section>
   );
